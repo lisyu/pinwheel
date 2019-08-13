669322b81918a29c61a6178edcd67813911436ca
lisyu <stella_li@brown.edu>
2019-08-13 14:22:39 -0400

    added message blacklisting feature

--- pinwheel.py
+++ pinwheel.py
@@ -19,9 +19,10 @@ SAVE_DELAY = 60 # in minutes
 FLAG = "p?"
 
 GREET_MSG = "> **Howdy! I'm Pinwheel!**"
-HELP_MSG = "> Available commands: `help` `status` `howdy` `setcount` `setemoji`"
+HELP_MSG = "> Available commands: `help` `status` `howdy` `setcount` `setemoji` `setignore`"
 DEV_MSG = "> Available dev commands: `savestate` `reset` `togglev` `lastlogin` `lastsave`"
 STATUS_MSG = "> Right now I'm looking for posts with at least `{}` {} reacts."
+BLACKLIST_MSG = "> I'm also ignoring all messages with {} reacts."
 
 PIN_EMOJI_DEFAULT = "📌"
 MIN_COUNT_DEFAULT = 2
@@ -52,15 +53,22 @@ class Pinwheel():
         # Pin management variables
         self.pin_emoji = PIN_EMOJI_DEFAULT
         self.pin_count = MIN_COUNT_DEFAULT
+        self.blacklist_emoji = None
 
     def get_greeting(self):
         return "{}\n{}".format(GREET_MSG, HELP_MSG)
 
     def get_status(self):
-        return STATUS_MSG.format(self.pin_count, self.pin_emoji)
+        if self.blacklist_emoji != None:
+            return STATUS_MSG.format(self.pin_count, self.pin_emoji) + "\n" + BLACKLIST_MSG.format(self.blacklist_emoji)
+        else:
+            return STATUS_MSG.format(self.pin_count, self.pin_emoji)
 
     def can_pin(self, reaction):
         return str(reaction.emoji) == self.pin_emoji and reaction.count == self.pin_count
+
+    def can_ignore(self, reaction):
+        return str(reaction.emoji) == self.blacklist_emoji
 
     def set_count(self, num):
         # print("Set minimum required reaction count to {}".format(num))
@@ -69,6 +77,10 @@ class Pinwheel():
     def set_emoji(self, s):
         # print("Set required reaction emoji to {}".format(s))
         self.pin_emoji = s
+        
+    def set_blacklist_emoji(self, s):
+        # print("Set blacklist message emoji to {}".format(s))
+        self.blacklist_emoji = s
         
     def is_valid_emoji(self, s, guild):
         return s in UNICODE_EMOJI or s in [str(emoji) for emoji in guild.emojis]
@@ -150,6 +162,19 @@ class PinClient(discord.Client):
                 await message.channel.send("Please enter a valid emoji!")
         except IndexError:
             await message.channel.send("**Usage**: {}setemoji `<new reaction emoji>`".format(FLAG))
+    
+    async def try_set_blacklist_emoji(self, message):
+        msg = message.content.split(" ")
+        try:
+            if msg[1] == "none":
+                self.get_config(message.guild.id).set_blacklist_emoji(None)
+            if self.get_config(message.guild.id).is_valid_emoji(msg[1], message.guild):
+                self.get_config(message.guild.id).set_blacklist_emoji(msg[1])
+                await message.channel.send("From now on, I'll ignore messages with {}.".format(msg[1]))
+            else:
+                await message.channel.send("Please enter a valid emoji!")
+        except IndexError:
+            await message.channel.send("**Usage**: {}setignore `<new blacklist emoji or 'none'>`".format(FLAG))
 
     async def try_pin(self, message):
         log("Pinning post #{} in {}...".format(message.id, message.guild.name))
@@ -198,13 +223,18 @@ class PinClient(discord.Client):
 
         # CHANGE REACTION REQ COUNT
         elif message.content.startswith("{}setcount".format(FLAG)):
-            # Only allow command for admins (TODO: change?)
+            # Only allow command for server managers 
             await self.try_admin_command(message, self.try_set_count)
             
         # CHANGE REACTION EMOJI
         elif message.content.startswith("{}setemoji".format(FLAG)):
-            # Only allow command for admins (TODO: change?)
+            # Only allow command for server managers 
             await self.try_admin_command(message, self.try_set_emoji)
+            
+        # CHANGE BLACKLIST EMOJI
+        elif message.content.startswith("{}setignore".format(FLAG)):
+            # Only allow command for server managers 
+            await self.try_admin_command(message, self.try_set_blacklist_emoji)
 
         ## DEV COMMANDS --------------------------------------------------------            
         # SAVE SESSIONMAP
@@ -237,11 +267,14 @@ class PinClient(discord.Client):
     async def on_raw_reaction_add(self, payload):
         channel = await self.fetch_channel(payload.channel_id)
         message = await channel.fetch_message(payload.message_id)
+        guild_session = self.get_config(payload.guild_id)
+
         if message.pinned == False:
-            for reaction in message.reactions:
-                if self.get_config(payload.guild_id).can_pin(reaction):
-                    await self.try_pin(message)
-                    return
+            if not any([guild_session.can_ignore(reaction) for reaction in message.reactions]):
+                for reaction in message.reactions:
+                    if guild_session.can_pin(reaction):
+                        await self.try_pin(message)
+                        return
 
             
 def main():              
